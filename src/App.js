741afb074e23479f70d43f7a694f936d8857d3c5
Rodney Nganga <rnganga2018@gmail.com>
2021-05-05 13:53:58 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Main from './components/Main';
+import Subcontent from './components/Subcontent';
+import Advertisement from './components/Advertisement';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center">
+    <div className="web">
+      <Header />
+      <div className="body">
+      <Navigation />
+      <Main className="main">
+        <div className="subContent">
+        <Subcontent />
+        <Subcontent />
+        <Subcontent />
+        </div>
+        <Advertisement />
+      </Main>
+      </div>
+    </div>
     </div>
   );
 }
---
+++ src/components/Subcontent.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import styles from './Subcontent.module.css';
+
+class Subcontent extends Component {
+    render() {
+        return (
+            <div className={styles.box}></div>
+        )
+    }
+}
+
+export default Subcontent;
